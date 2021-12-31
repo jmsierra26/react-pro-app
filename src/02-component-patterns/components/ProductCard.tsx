f3734dccaf96090f7fc6d07eafda3e01118f3d88
Jesús Sierra <dev.jesus.sierra@gmail.com>
2021-12-31 16:37:51 -0500

    Fin del patrón.

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,6 +1,6 @@
-import { createContext, CSSProperties, ReactElement } from 'react'
+import { createContext, CSSProperties } from 'react'
 import { useProduct } from '../hooks/useProduct'
-import { onChangeArgs, Product, ProductContextProps } from '../interfaces/interfaces';
+import { InitialValues, onChangeArgs, Product, ProductContextProps, ProductCardHandler } from '../interfaces/interfaces';
 
 import styles from '../styles/styles.module.css'
 
@@ -8,28 +8,39 @@ export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
 
 export interface Props {
-  children?: ReactElement | ReactElement[];
+  // children?: ReactElement | ReactElement[];
+  children: (args: ProductCardHandler) => JSX.Element;
   product: Product;
   className?: string;
   style?: CSSProperties;
   onChange?: (args: onChangeArgs) => void;
   value?: number;
+  initialValues?: InitialValues;
 }
 
-export const ProductCard = ({ children, product, className, style, onChange, value }: Props) => {
-  const { counter, increaseBy } = useProduct({ product, onChange, value })
+export const ProductCard = ({ children, product, className, style, onChange, value, initialValues }: Props) => {
+  const { counter, increaseBy, maxCount, isMaxCountReached, reset } =
+    useProduct({ product, onChange, value, initialValues })
 
   return (
     <Provider value={{
       counter,
       increaseBy,
+      maxCount,
       product,
     }}>
       <div
         className={`${styles.productCard} ${className}`}
         style={style}
       >
-        {children}
+        {children({
+          count: counter,
+          isMaxCountReached,
+          maxCount: initialValues?.maxCount,
+          product,
+          increaseBy,
+          reset,
+        })}
       </div>
     </Provider>
   )
